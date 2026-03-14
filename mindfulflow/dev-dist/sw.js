caf0bc081b6ccb5141f9f8454395bc8214bf2e84
tickly50 <tomasskala03@gmail.com>
2026-03-14 13:23:40 +0100

    update

--- mindfulflow/dev-dist/sw.js
+++ mindfulflow/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4js0onvpels"
+    "revision": "0.s3a76ffl6tc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
