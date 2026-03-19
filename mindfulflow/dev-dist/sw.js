0e75cdadc23c26a4f60589f888247ea4eb098561
tickly50 <tomasskala03@gmail.com>
2026-03-19 16:54:35 +0100

    Optimize animation handling in CheckInView and ContextTags for enhanced performance and user experience
    - Refactored CheckInView to improve scroll behavior and animation transitions when selecting moods.
    - Simplified ContextTags by removing unnecessary motion wrappers, leading to better performance and cleaner code.
    - Updated styles to ensure a consistent experience across components while maintaining accessibility standards.

--- mindfulflow/dev-dist/sw.js
+++ mindfulflow/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6j1oh0e93v"
+    "revision": "0.jeupufj0kec"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
